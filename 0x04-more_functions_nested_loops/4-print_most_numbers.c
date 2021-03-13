905d030133fb5d9b894e5e19fa93452098241aad
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-03-13 12:23:11 +0000

    prints the numbers, from 0 to 9 without the 2 and the 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers -  prints the numbers from 0 to 9,
+ *                        without the 2 and the 4.
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		if (n != '2' &&  n != '4')
+		{
+			_putchar(n);
+		}
+
+	}
+
+	_putchar('\n');
+}
