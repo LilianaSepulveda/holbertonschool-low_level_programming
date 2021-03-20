3c38d3efedb00bce73c8d70e22004e980a1ef467
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-03-20 01:26:29 +0000

    encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * leet - Encodes a string to 1337.
+ * @st: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *leet(char *st)
+{
+	int r, x;
+
+	char leet[10] = "aAeEoOtTlL";
+	char num[10] = "4433007711";
+
+
+	for (r = 0; st[r] != '\0'; r++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			if (st[r] == leet[x])
+			{
+				st[r] = num[x];
+			}
+		}
+
+	}
+	return (st);
+}
