74e78878df08f123d285307da49c25ce59f6e115
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-03-30 00:41:52 +0000

    list of the prototypes of the functions used in project

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,25 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+
+/**
+ *
+ *content of the prototypes of the functions used in project 0x15. C - File I/O
+ *           file descriptors.
+ *
+ */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+
+
+#endif /*HOLBERTONH*/
